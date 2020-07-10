4275f24114f9895d0858863087ff16e96059e336
Rohit Anand <rohit.anand3456@gmail.com>
2020-07-11 00:13:29 +0530

    Basic folder structure,created empty components file

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React ,{Component,useEffect,useState} from 'react';
+import Classes from './App.module.css';
+
+class App extends Component{
+  render(){
+    return(
+      <div className={Classes.App}>
+        
+      </div>
+    )
+  }
+}
+
+export default App;
